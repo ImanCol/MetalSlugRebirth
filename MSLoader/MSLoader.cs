b5febdd5d6ea08807233798195597958a643f4a7
ImanCol <marlon_infinito@hotmail.com>
2025-04-21 20:17:05 -0500

    simple update

--- MSLoader/MSLoader.cs
+++ MSLoader/MSLoader.cs
@@ -1,26 +1,112 @@
-// ReSharper disable UnusedType.Global
-// ReSharper disable UnusedMember.Global
 
+
+using System.Reflection;
 using BepInEx.Logging;
-using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
+using MSALoader.Components;
+using MSALoader.Core;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public static class MSLoader
 {
-    private static ManualLogSource log;
-
-    // UnityEngine.Debug causes System.AccessViolationException exception, inject ManualLogSource as a workaround
-    public static void Main(ManualLogSource log)
+    public static ManualLogSource Logger;
+    public static Harmony _harmony;
+    public static async Task Main(ManualLogSource log)
     {
+        HarmonyHooks();
+        await Task.Delay(3000);
+
         log.LogInfo("MSLoader.Main invoked.");
-        MSLoader.log = log;
+        Logger = log;
+        Logger.LogWarning("Creando objeto...");
+
+        if (MSBootstrap.Instance == null)
+        {
+            RegisterAssembly();
+            MakeObject();
+        }
+        else
+        {
+            Logger.LogWarning("MSBootstrap ya existe, removiendo...");
+            UnityEngine.Object.Destroy(MSBootstrap.Instance);
+            //RegisterAssembly();
+            MakeObject();
+        }
     }
+
+    public static void MakeObject()
+    {
+        GameObject gameObject = new GameObject("MSALoader");
+        UnityEngine.Object.DontDestroyOnLoad(gameObject);
+        Logger.LogWarning("Agregando MSBootstrap...");
+        MSBootstrap.Instance = gameObject.AddComponent<MSBootstrap>();
+    }
+    public static void RegisterAssembly()
+    {
+
+        Logger.LogWarning("Encontrado.");
+        if (!ClassInjector.IsTypeRegisteredInIl2Cpp<MSBootstrap>())
+        {
+            Logger.LogWarning("MSBootstrap ya existe, removiendo...");
+            UnityEngine.Object.Destroy(MSBootstrap.Instance);
+            Logger.LogWarning("Registrando MSBootstrap...");
+            ClassInjector.RegisterTypeInIl2Cpp<MSBootstrap>();
+        }
+
+        if (!ClassInjector.IsTypeRegisteredInIl2Cpp<CameraDebugController>())
+        {
+            Logger.LogWarning("Registrando CameraDebugController...");
+            ClassInjector.RegisterTypeInIl2Cpp<CameraDebugController>();
+        }
+
+    }
+
+    public enum HookType
+    {
+        Pre,
+        Post,
+        Transpiler
+    }
+
+    public static void Hookear(Type _metrodInfo, string _internalMethod, Type _harmonyMethod, string _externalMethod, HookType _hookType = HookType.Pre)
+    {
+        var _Method = _metrodInfo
+            .GetMethod(_internalMethod, BindingFlags.Public | BindingFlags.Instance);
+
+        if (_Method != null)
+        {
+            var prefix = _harmonyMethod.GetMethod(_externalMethod);
+
+            //tipo de hook
+            if (_hookType == HookType.Pre)
+            {
+                _harmony.Patch(_Method, prefix: new HarmonyMethod(prefix));
+            }
+            else if (_hookType == HookType.Post)
+            {
+                _harmony.Patch(_Method, postfix: new HarmonyMethod(prefix));
+            }
+            else if (_hookType == HookType.Transpiler)
+            {
+                _harmony.Patch(_Method, transpiler: new HarmonyMethod(prefix));
+            }
+
+            Logger.LogInfo($"Hook para {_internalMethod} aplicado");
+        }
+    }
+
+    public static void HarmonyHooks()
+    {
+        _harmony = new Harmony("com.tu.nombre.packetsniffer.il2cpp");
+        //AssetBundleHook.Hook();
+    }
+
 
     public static void Unload()
     {
         // Unload and unpatch everything before reloading the script
-        log.LogInfo("MSLoader.Unload invoked.");
+        Logger.LogInfo("MSLoader.Unload invoked.");
     }
 
 }

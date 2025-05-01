fc13f3bae9823c6b637929aaa9a733acdc52bf5f
ImanCol <marlon_infinito@hotmail.com>
2025-05-01 08:52:49 -0500

    Migrate Static

--- AssemblyLoader/BepInExLoader.cs
+++ AssemblyLoader/BepInExLoader.cs
@@ -3,15 +3,24 @@ using BepInEx.Unity.IL2CPP;
 
 namespace AssemblyLoader
 {
+
     [BepInPlugin("BepInEx.IL2CPP.AssemblyLoader", "AssemblyLoader", "1.0.0")]
     public class BepInExLoader : BasePlugin
     {
+
+        //fix lastest version?
+        private AssemblyLoader assemblyLoader;
+
         public override void Load()
         {
             Log.LogInfo($"Plugin AssemblyLoader is loaded!");
 
             Bootstrap.Log = Log;
-            AddComponent<Bootstrap>();
+
+            //fix lastest version?
+            assemblyLoader = new AssemblyLoader(Log);
+
+            //AddComponent<Bootstrap>();
         }
     }
 }
